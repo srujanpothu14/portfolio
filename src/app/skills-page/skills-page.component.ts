222e0bc469477da8366b4484644e432611f97588
srujanpothu14 <srujanpothu@outlook.com>
2025-03-05 23:43:50 +0530

    made changes in projects page and added skills to the skills page

--- src/app/skills-page/skills-page.component.ts
+++ src/app/skills-page/skills-page.component.ts
@@ -4,8 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-skills-page',
   imports: [],
   templateUrl: './skills-page.component.html',
-  styleUrl: './skills-page.component.scss'
+  styleUrl: './skills-page.component.scss',
 })
 export class SkillsPageComponent {
-
+  skills = [
+    { name: 'HTML', logo: 'assets/html_logo.svg' },
+    { name: 'CSS', logo: 'assets/css_logo.svg' },
+    { name: 'JavaScript', logo: 'assets/javascript_logo.svg' },
+    { name: 'TypeScript', logo: 'assets/typescript_logo.svg' },
+    { name: 'Angular', logo: 'assets/angular_logo.svg' },
+    { name: 'Node.js', logo: 'assets/nodejs_logo.svg' },
+    { name: 'Express.js', logo: 'assets/express_logo.png' },
+    { name: 'DynamoDB', logo: 'assets/dynamodb_logo.svg' },
+    { name: 'C#', logo: 'assets/csharp_logo.svg' },
+    { name: 'Java', logo: 'assets/java_logo.svg' },
+    { name: 'Selenium', logo: 'assets/selenium_logo.svg' },
+  ];
 }
