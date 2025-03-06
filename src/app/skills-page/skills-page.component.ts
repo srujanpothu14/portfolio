1c63e42140853dfc947251fe5dab078eab594c89
srujanpothu14 <srujanpothu@outlook.com>
2025-03-06 23:40:52 +0530

    rearranged skills

--- src/app/skills-page/skills-page.component.ts
+++ src/app/skills-page/skills-page.component.ts
@@ -8,14 +8,14 @@ import { Component } from '@angular/core';
 })
 export class SkillsPageComponent {
   skills = [
-    { name: 'HTML', logo: 'assets/html_logo.svg' },
-    { name: 'CSS', logo: 'assets/css_logo.svg' },
-    { name: 'JavaScript', logo: 'assets/javascript_logo.svg' },
-    { name: 'TypeScript', logo: 'assets/typescript_logo.svg' },
     { name: 'Angular', logo: 'assets/angular_logo.svg' },
     { name: 'Node.js', logo: 'assets/nodejs_logo.svg' },
     { name: 'Express.js', logo: 'assets/express_logo.png' },
     { name: 'DynamoDB', logo: 'assets/dynamodb_logo.svg' },
+    { name: 'HTML', logo: 'assets/html_logo.svg' },
+    { name: 'CSS', logo: 'assets/css_logo.svg' },
+    { name: 'JavaScript', logo: 'assets/javascript_logo.svg' },
+    { name: 'TypeScript', logo: 'assets/typescript_logo.svg' },
     { name: 'C#', logo: 'assets/csharp_logo.svg' },
     { name: 'Java', logo: 'assets/java_logo.svg' },
     { name: 'Selenium', logo: 'assets/selenium_logo.svg' },
