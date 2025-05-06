5fd6703f74bd5b2133a524b67b203fb063af61fe
srujan1306 <srujan.pothu@proclink.com>
2025-05-06 17:41:58 +0530

    made border changes and display flex is set in hero tabs container in app scss component.

--- src/app/experience-page/experience-page.component.ts
+++ src/app/experience-page/experience-page.component.ts
@@ -1,23 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience-page.component.html',
   styleUrl: './experience-page.component.scss',
 })
 export class ExperiencePageComponent {
-  activeContent: string = 'proclink'; // Default Content
-  proclink_experience: string[] = [
-    'Developed and executed automated test scripts using Selenium and C# to ensure the reliability and efficiency of web applications, improving test coverage and reducing manual effort.',
-    'Designed and maintained a robust test automation framework, streamlining the testing process and enhancing overall software quality.',
-    'Designed and developed pdf generation feature for the application using iTextSharp and Oxyplot libraries in C# to generate test reports.',
-    'Got trained in MEAN Stack (MongoDB, Express.js, Angular, and Node.js) to expand expertise in full-stack development.',
-    'Single-handedly developed a full-stack web application using the MEAN stack, implementing scalable backend services and an interactive frontend UI to streamline internal processes.',
-    'Optimized API performance and database queries, ensuring efficient data retrieval and seamless user interactions.',
+  experiences = [
+    {
+      company: 'Proclink Consulting Services LLP',
+      role: 'Graduate Engineer Trainee',
+      duration: 'May 2024 - April 2025',
+      description:
+        'Worked on Angular-based SPA applications and UI improvements.',
+    },
+    {
+      company: 'Proclink Consulting Services LLP',
+      role: 'Associate Software Engineer',
+      duration: 'April 2025 - Present',
+      description:
+        'Worked on Angular-based SPA applications and UI improvements.',
+    },
   ];
-
-  setActiveContent(content: string) {
-    this.activeContent = content;
-  }
 }
